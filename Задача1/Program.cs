58b6ea4172a821703b65500b53fffd0650204043
Alina <Asdfrt21h@gmail.com>
2023-09-14 22:38:05 +0300

    gdfyg

---
+++ Задача1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+using System;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    int[,] arr = {
+      {10, 4, 6, 2},
+      {5, 9, 2, 3},
+      {8, 40, 2, 4}
+    };
+
+    printArray(arr);
+
+    int row = arr.GetUpperBound(0) + 1;
+    int cal = arr.Length / row;
+
+    for (int i = 0; i < row; i++)
+      for (int j = 0; j < cal; j++)
+        for (int x = 0; x < cal; x++)
+          for (int y = x; y < cal; y++)
+            if (arr[i, x] < arr[i, y])
+            {
+              arr[i, y] ^= arr[i, x];
+              arr[i, x] ^= arr[i, y];
+              arr[i, y] ^= arr[i, x];
+            }
+            
+    printArray(arr);
+  }
+
+  public static void printArray(int[,] arr)
+  {
+    Console.WriteLine();
+    int row = arr.GetUpperBound(0) + 1;
+    int cal = arr.Length / row;
+
+    for (int i = 0; i < row; i++)
+    {
+      for (int j = 0; j < cal; j++)
+        Console.Write($"{arr[i, j]} ");
+      Console.WriteLine();
+    }
+  }
+} 
---
+++ Задача3/Program.cs
@@ -0,0 +1,66 @@
+﻿/*задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+
+2 4 | 3 4
+3 2 | 3 3 
+Результирующая матрица будет:
+
+18 20
+15 18 
+*/
+using System;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    int[,] A = {
+      {2, 4},
+      {3, 2}
+    };
+
+    int[,] B = {
+      {3, 4},
+      {3, 3}
+    };
+
+    int rowA = A.GetUpperBound(0) + 1;
+    int colA = A.Length / rowA;
+
+    int rowB = B.GetUpperBound(0) + 1;
+    int colB = B.Length / rowB;
+
+    int[,] C = new int[rowB, colB];
+
+    if (colA == rowB)
+    {
+      for (int i = 0; i < rowB; i++)
+        for (int l = 0; l < colA; l++)
+          for (int j = 0; j < colB; j++)
+            C[l, j] += A[l, i] * B[i, j];
+    }
+    else
+    {
+      Console.WriteLine($"Такую матрицу перемножать нельзя");
+    }
+
+    printArray(A);
+    printArray(B);
+    printArray(C);
+  }
+
+  private static void printArray(int[,] arr)
+  {
+    Console.WriteLine();
+
+    int row = arr.GetUpperBound(0) + 1;
+    int col = arr.Length / row;
+
+    for (int i = 0; i < row; i++)
+    {
+      for (int j = 0; j < col; j++)
+        Console.Write($"{arr[i, j]} ");
+      Console.WriteLine();
+    }
+  }
+} 
